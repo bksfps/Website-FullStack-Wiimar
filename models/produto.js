f9af8d81776c98c1dbe894ac3056a54afca70b10
Bruno Kenji Saito <bruno.kenji15@gmail.com>
2024-11-12 19:06:38 -0300

    ver 1.0

--- models/produto.js
+++ models/produto.js
@@ -5,7 +5,8 @@ const produtoSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     descricao: { type: String, required: true },
     imagem: { type: String, required: true },
-    preco: { type: Number, required: true }
+    preco: { type: Number, required: true },
+    categoria: { type: String, required: true }
 });
 
 const Produto = mongoose.model('Produto', produtoSchema);
